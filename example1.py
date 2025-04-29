a705110ac3a7b329c73e5356faba65cb0d2efe3a
Escobar <tl4583@mci4me.at>
2025-04-29 15:55:29 +0200

    add load_data

---
+++ example1.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+my_array = np.array([1, 2, 3])
+print(my_array)
+
+# This is a simple example of using numpy to create an array.
---
+++ power_curve.py
@@ -0,0 +1,10 @@
+import matplotlib as plt
+
+from load_data import load_data
+from sort import bubble_sort
+
+data = load_data('activity.csv')
+power_W = data['PowerOriginal']
+print(power_W)
+sorted_power_W = bubble_sort(power_W)
+print(sorted_power_W[::-1])
